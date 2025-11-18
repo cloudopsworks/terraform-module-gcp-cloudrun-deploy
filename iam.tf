88b68e898b0eb0453dcce262dcdee6729de117f3
Cristian Beraha <cristian@cloudops.works>
2025-11-18 19:22:12 -0300

    PIP-8 Refactor IAM bindings to simplify `deployment_sa` handling and correct resource definitions.

--- iam.tf
+++ iam.tf
@@ -31,21 +31,21 @@ resource "google_project_iam_member" "registry_writer" {
   member  = google_service_account.cloudrun_sa.member
 }
 
-resource "google_project_iam_member" "act_as" {
-    project = google_service_account.cloudrun_sa.project
-    role    = "roles/iam.serviceAccountUser"
-    member  = google_service_account.cloudrun_sa.member
+# resource "google_project_iam_member" "act_as" {
+#     project = google_service_account.cloudrun_sa.project
+#     role    = "roles/iam.serviceAccountUser"
+#     member  = google_service_account.cloudrun_sa.member
+# }
+data "google_service_account" "deployment_sa" {
+  count      = var.deployment_sa != "" ? 1 : 0
+  account_id = var.deployment_sa
 }
-# data "google_service_account" "deployment_sa" {
-#   count      = var.deployment_sa != "" ? 1 : 0
-#   account_id = var.deployment_sa
-# }
-#
-# resource "google_service_account_iam_binding" "sa_user" {
-#   count              = var.deployment_sa != "" ? 1 : 0
-#   service_account_id = data.google_service_account.deployment_sa[0].id
-#   role               = "roles/iam.serviceAccountUser"
-#   members            = [google_service_account.cloudrun_sa.member]
-# }
+
+resource "google_service_account_iam_binding" "sa_user" {
+  count              = var.deployment_sa != "" ? 1 : 0
+  service_account_id = google_service_account.cloudrun_sa.id
+  role               = "roles/iam.serviceAccountUser"
+  members            = [data.google_service_account.deployment_sa[0].member]
+}
 #
 #
